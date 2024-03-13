200b4083fbac2dafcba4c94ade3e79067f7c970e
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-13 14:46:47 +0300

    Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,12 +3,12 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {}
+                () -> { }
         );
         System.out.println(first.getState());
         first.start();
         Thread second = new Thread(
-                () -> {}
+                () -> { }
         );
         System.out.println(second.getState());
         second.start();
