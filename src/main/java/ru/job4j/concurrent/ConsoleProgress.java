2a9c54e04f0347ac9a51ab4df67b7e9420b44304
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-13 22:20:20 +0300

    Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        for (int i = 0; i < process.length; i++) {
+            Thread.currentThread().isInterrupted();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print("\r load: " + process[i]);
+        }
+    }
+}
