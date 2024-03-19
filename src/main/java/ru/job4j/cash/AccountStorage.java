76f122e59d7e02098dff231c5b7103a74b756478
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-19 15:00:44 +0300

    Денежные переводы AccountStorage

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,44 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import java.util.HashMap;
+import java.util.Optional;
+
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+    private int id = 1;
+
+    public synchronized boolean add(Account account) {
+        accounts.putIfAbsent(id++, account);
+        return true;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), accounts.get(account.id()), account);
+    }
+
+    public synchronized void delete(int id) {
+        accounts.remove(id);
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        boolean rsl = false;
+            Account fromAccount = accounts.get(fromId);
+            Account toAccount = accounts.get(toId);
+        if (fromAccount != null && toAccount != null && fromAccount.amount() >= amount) {
+            fromAccount = new Account(fromAccount.id(), fromAccount.amount() - amount);
+            toAccount = new Account(toAccount.id(), toAccount.amount() + amount);
+            accounts.put(fromId, fromAccount);
+            accounts.put(toId, toAccount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
