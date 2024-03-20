cc386771b8b1658a256faac30a0ee21c8e4c9f9e
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-20 13:08:36 +0300

    Visibility. Общий ресурс вне критической секции

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -11,17 +11,25 @@ public class ParseFile {
     }
 
    public synchronized String content(Predicate<Character> filter) throws IOException {
-       String output = "";
+       StringBuilder sb = new StringBuilder();
        try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
            int data;
-           while ((data = input.read()) > 0) {
+           while ((data = input.read()) != -1) {
                if (filter.test((char) data)) {
-                   output += (char) data;
+                   sb.append(data);
                }
            }
        } catch (Exception e) {
            e.printStackTrace();
        }
-       return output;
+       return sb.toString();
    }
+
+    public String getContent() throws IOException {
+        return content(data -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return content(data -> data < 0x80);
+    }
 }
