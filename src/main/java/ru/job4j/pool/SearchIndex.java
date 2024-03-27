cc9d6e0beb7533687256bf642c0a36236f26cfc5
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-27 13:26:47 +0300

    ForkJoinPool

--- src/main/java/ru/job4j/pool/SearchIndex.java
+++ src/main/java/ru/job4j/pool/SearchIndex.java
@@ -1,14 +1,15 @@
 package ru.job4j.pool;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class SearchIndex extends RecursiveTask<Integer> {
-    private final int[] array;
-    private final int searchElement;
+public class SearchIndex<T> extends RecursiveTask<Integer> {
+    private final T[] array;
+    private final T searchElement;
     private final int first;
     private final int last;
 
-    public SearchIndex(int[] array, int searchElement, int first, int last) {
+    public SearchIndex(T[] array, T searchElement, int first, int last) {
         this.array = array;
         this.searchElement = searchElement;
         this.first = first;
@@ -18,24 +19,32 @@ public class SearchIndex extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if (last - first < 10) {
-            for (int i = first; i <= last; i++) {
-                if (searchElement == array[i]) {
-                    return i;
-                }
-            }
-            return -1;
+            return search(first, last);
         }
         int middle = (first + last) / 2;
         SearchIndex left = new SearchIndex(array, searchElement, first, middle);
         SearchIndex right = new SearchIndex(array, searchElement, middle, last);
         left.fork();
         right.fork();
-        int leftRsl = left.join();
-        int rightRsl = right.join();
-        if (leftRsl != -1) {
-            return leftRsl;
-        } else {
-            return rightRsl;
-        }
+        T leftRsl = (T) left.join();
+        T rightRsl = (T) right.join();
+        T t = (Integer) leftRsl != -1 ? leftRsl : rightRsl;
+        return (Integer) t;
     }
+
+     private int search(int first, int last) {
+         for (int i = first; i <= last; i++) {
+             if (searchElement == array[i]) {
+                 return i;
+             }
+         }
+         return -1;
+     }
+
+
+     public static <T> int startSearchIndex(T[] array, T el) {
+         ForkJoinPool fjp = new ForkJoinPool();
+         return fjp.invoke(new SearchIndex<>(array, el, 0, array.length - 1));
+    }
+
 }
--- src/test/java/ru/job4j/pool/SearchIndexTest.java
+++ src/test/java/ru/job4j/pool/SearchIndexTest.java
@@ -7,28 +7,37 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class SearchIndexTest {
     @Test
             void whenSearchIntElement() {
-        int[] array = {1, 5, 8, 7, 6, 0, 8, 6, 9, 10, 58, 4, 35};
-        int element = 10;
+        Integer[] array = {1, 5, 8, 7, 6, 0, 8, 6, 9, 10, 58, 4, 35};
+        Integer element = 10;
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        int expected = forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
+        int expected = (int) forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
         assertThat(expected).isEqualTo(9);
     }
 
     @Test
     void whenSearchIntElementMin() {
-        int[] array = {1, 5, 8, 7};
-        int element = 5;
+        Integer[] array = {1, 5, 8, 7};
+        Integer element = 5;
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        int expected = forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
+        int expected = (int) forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
         assertThat(expected).isEqualTo(1);
     }
 
     @Test
     void whenSearchIntElementIsNot() {
-        int[] array = {1, 5, 8, 7, 7, 9, 40, 65, 8, 77, 30, 4, 0, 9};
-        int element = 100;
+        Integer[] array = {1, 5, 8, 7, 7, 9, 40, 65, 8, 77, 30, 4, 0, 9};
+        Integer element = 100;
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        int expected = forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
+        int expected = (int) forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
         assertThat(expected).isEqualTo(-1);
+    }
+
+    @Test
+    void whenSearchStringElement() {
+        String[] array = {"petr", "oljga", "dog", "cat", "pool", "milk"};
+        String element = "pool";
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        int expected = (int) forkJoinPool.invoke(new SearchIndex(array, element, 0, array.length - 1));
+        assertThat(expected).isEqualTo(4);
     }
 }
