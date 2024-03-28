a85fe54e02c5661d07ce490c0a7a8110a4956b0f
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-28 13:01:31 +0300

    ForkJoinPool

--- src/main/java/ru/job4j/pool/SearchIndex.java
+++ src/main/java/ru/job4j/pool/SearchIndex.java
@@ -26,10 +26,7 @@ public class SearchIndex<T> extends RecursiveTask<Integer> {
         SearchIndex right = new SearchIndex(array, searchElement, middle, last);
         left.fork();
         right.fork();
-        T leftRsl = (T) left.join();
-        T rightRsl = (T) right.join();
-        T t = (Integer) leftRsl != -1 ? leftRsl : rightRsl;
-        return (Integer) t;
+        return Math.max((Integer) left.join(), (Integer) right.join());
     }
 
      private int search(int first, int last) {
@@ -41,10 +38,8 @@ public class SearchIndex<T> extends RecursiveTask<Integer> {
          return -1;
      }
 
-
      public static <T> int startSearchIndex(T[] array, T el) {
          ForkJoinPool fjp = new ForkJoinPool();
          return fjp.invoke(new SearchIndex<>(array, el, 0, array.length - 1));
     }
-
 }
