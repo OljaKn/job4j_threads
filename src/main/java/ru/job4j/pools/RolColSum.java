8596af8263eea2eb5c7d42faf7389e89d7b8a434
origin <git@github.com:OljaKn/job4j_design.git>
2024-03-28 13:32:42 +0300

    CompletableFuture

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,25 +1,9 @@
 package ru.job4j.pools;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public Sums(int rowSum, int colSum) {
-            this.rowSum = rowSum;
-            this.colSum = colSum;
-        }
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         int n = matrix.length;
@@ -55,5 +39,40 @@ public class RolColSum {
             colSum += matrix[j][index];
         }
         return new Sums(rowSum, colSum);
+    }
+}
+
+class Sums {
+    private int rowSum;
+    private int colSum;
+
+    public Sums(int rowSum, int colSum) {
+        this.rowSum = rowSum;
+        this.colSum = colSum;
+    }
+
+    public int getRowSum() {
+        return rowSum;
+    }
+
+    public int getColSum() {
+        return colSum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Sums sums = (Sums) o;
+        return rowSum == sums.rowSum && colSum == sums.colSum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rowSum, colSum);
     }
 }
--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -2,33 +2,41 @@ package ru.job4j.pools;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RolColSumTest {
     @Test
-    public void whenComputeSum() {
+    public void testSum() {
         int[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        RolColSum.Sums[] sums = RolColSum.sum(matrix);
-        assertThat(sums[0].getRowSum()).isEqualTo(6);
-        assertThat(sums[0].getColSum()).isEqualTo(12);
-        assertThat(sums[1].getRowSum()).isEqualTo(15);
-        assertThat(sums[1].getColSum()).isEqualTo(15);
+        Sums[] expected = {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)
+        };
+        Sums[] rsl = RolColSum.sum(matrix);
+        assertEquals(expected.length, rsl.length);
+        assertThat(expected[0]).isEqualTo(rsl[0]);
     }
 
     @Test
-    public void whenComputeAsyncSum() {
+    public void testAsyncSum() {
         int[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        RolColSum.Sums[] sums = RolColSum.asyncSum(matrix);
-        assertThat(sums[0].getRowSum()).isEqualTo(6);
-        assertThat(sums[0].getColSum()).isEqualTo(12);
-        assertThat(sums[1].getRowSum()).isEqualTo(15);
-        assertThat(sums[1].getColSum()).isEqualTo(15);
+        Sums[] expected = {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)
+        };
+        Sums[] rsl = RolColSum.asyncSum(matrix);
+        assertEquals(expected.length, rsl.length);
+        assertThat(expected[1]).isEqualTo(rsl[1]);
+        assertThat(expected[2]).isEqualTo(rsl[2]);
     }
 }
